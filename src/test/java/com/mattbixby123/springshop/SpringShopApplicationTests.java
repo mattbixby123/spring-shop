863daf3937de63479575551eb1674e97c2402e0c
mattbixby123 <matthew.bixby1@gmail.com>
2024-12-06 16:43:05 -0500

    testing

---
+++ src/test/java/com/mattbixby123/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattbixby123.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
