ba6e03fc779b4c58b37f0dddffe4ff188aa7f413
mattbixby123 <matthew.bixby1@gmail.com>
2024-12-07 18:09:53 -0500

    updates to Product model

--- src/main/java/com/mattbixby123/springshop/model/Product.java
+++ src/main/java/com/mattbixby123/springshop/model/Product.java
@@ -1,20 +1,13 @@
 package com.mattbixby123.springshop.model;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
